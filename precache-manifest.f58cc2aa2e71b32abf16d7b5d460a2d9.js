3cdb005663268555b3d38e4cc1760cc42d1bb984
Vincent Battaglia <vpbattaglia@gmail.com>
2020-05-03 16:50:59 -0700

    Updates

---
+++ precache-manifest.f58cc2aa2e71b32abf16d7b5d460a2d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d1f571e12c697d5a1aafeb2140c64a",
+    "url": "/New-Keeper-App/index.html"
+  },
+  {
+    "revision": "8633f8e1fbcdca8237f7",
+    "url": "/New-Keeper-App/static/css/main.fc54a75d.chunk.css"
+  },
+  {
+    "revision": "94cf2ea1ee38967b8440",
+    "url": "/New-Keeper-App/static/js/2.6966fcb2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/New-Keeper-App/static/js/2.6966fcb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8633f8e1fbcdca8237f7",
+    "url": "/New-Keeper-App/static/js/main.bc79e1a6.chunk.js"
+  },
+  {
+    "revision": "0c4281c8b6cd8775571e",
+    "url": "/New-Keeper-App/static/js/runtime-main.49f9eba7.js"
+  }
+]);
